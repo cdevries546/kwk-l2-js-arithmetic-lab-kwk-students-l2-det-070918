8405dedfb31ebf4e537dc93fbb28885842918afd
cdevries546 <cdevries@comcast.net>
2018-07-12 20:11:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+// - When we started out, we assigned ID numbers sequentially to new users, so our
+// first user's ID is `1`, second is `2`, third is `3`, and so on. That was fine
+// when we were a fledgling company, but, now that we have millions of active
+// users, it would be easier if all of our user IDs were the same length. We'd like
+// the IDs to start from `1000000001` (one billion and one) instead of `1`. Create
+// a variable named `newID` that adds `1000000000` to the value in `oldID`, which
+// you shouldn't define — it will be provided for you in the test suite.
+
+let counter = 1;
+
+
 /*
 Variables defined in the test suite:
 - oldID
